a63fa90f9f8674ea50f2db52ed91764f82eea8a6
iqbal <149692277+IqbalSHD@users.noreply.github.com>
2023-12-24 23:07:08 +0800

    Update randomForest.py

--- randomForest.py
+++ randomForest.py
@@ -1,3 +1,6 @@
+from decisionTree import DecisionTree
+
+
 class RandomForest:
     def __init__(self, n_estimators=100, max_depth=None, min_samples_split=2, n_features=None):
         self.n_estimators = n_estimators
